411c966de3b08e9cd795b8090872c732da389489
Thirumal Ravula <travula@gmail.com>
2017-03-21 13:05:48 +0530

    init and make cleaned up

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 if [ -d literate-tools ]; then
     echo "literate-tools already present"
-    (cd literate-tools; git pull; git checkout readtheorg)
+    (cd literate-tools; git pull)
 else
     git clone https://github.com/vlead/literate-tools.git
-    (cd literate-tools; git checkout readtheorg)
+    (cd literate-tools)
 fi
 
 if [ -L tangle-make ]; then
